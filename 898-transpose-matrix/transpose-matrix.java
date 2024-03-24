8f40085e0b42be0081a6dadd42e38af45e1ae630
Samyak jain <samyakjn000@gmail.com>
2024-03-24 10:49:17 +0530

    Time: 1 ms (29.48%) | Memory: 44.8 MB (34.88%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.java
+++ 898-transpose-matrix/transpose-matrix.java
@@ -1,20 +1,15 @@
 class Solution {
     public int[][] transpose(int[][] matrix) {
-
-        //storing row length of original matrix
-        int m=matrix.length;
-        //storing column length of original length
-        int n=matrix[0].length;
-        //new matrix (temp) of dimensions n x m (original matrix dimension m x n)
-        int[][] temp=new int[n][m];
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                temp[j][i]=matrix[i][j];
-
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] newm = new int[n][m];
+        for(int i=0;i<m;i++){
+            for(int j = 0 ;j<n;j++){
+                 newm[j][i] =  matrix[i][j];
+         
             }
         }
-        return temp;
+        return newm;
+        
     }
 }
