cbdc2f649956bdbcdafc7d199648116f008b9f9c
samyakjn19 <samyakjn000@gmail.com>
2024-04-14 13:19:56 +0530

    first code

--- todaysQ/first.java
+++ todaysQ/first.java
@@ -1,8 +1,11 @@
-package market.DSA-SOLVED-PROBLEMS.todaysQ;
+
 
 public class first {
     public static void main(String[] args) {
         System.out.println("jingle well");
+        for(int i =0;i<9;i++){
+            System.out.println(2*i);
+        }
     }
     
 }
