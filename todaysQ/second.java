1d57a8e16dd7f661b3b1f6571574bbed803f7191
samyakjn19 <samyakjn000@gmail.com>
2024-04-14 14:15:28 +0530

    adding all

---
+++ todaysQ/second.java
@@ -0,0 +1,11 @@
+public class second {
+    public static void main(String[] args) {
+        
+        int i =0;
+         while(i<100){
+            System.out.println(" i am best");
+            i++;
+         }
+    }
+    
+}
